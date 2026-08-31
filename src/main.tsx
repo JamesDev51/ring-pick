011b4f8eb89748bc1bbd71c5d36bbbe8a09e41c2
정민석(James) <minseok.jeong.james@gmail.com>
2026-08-31 16:46:16 +0900

    perf: remove heavy webfont payload on mobile

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import '@fontsource/pretendard/400.css';
-import '@fontsource/pretendard/500.css';
-import '@fontsource/pretendard/600.css';
-import '@fontsource/pretendard/700.css';
 import './styles/tokens.css';
 import './styles/global.css';
 import { App } from './app/App';
